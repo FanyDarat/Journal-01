ba32baf995217b80ecc4c953573b628fca33280a
FanyDarat <rafaelabednego123@gmail.com>
2023-09-27 09:12:32 +0700

    Journal-01

---
+++ Biodata.java
@@ -0,0 +1,7 @@
+public class Biodata {
+    public static void main(String[] args) {
+        System.out.println("NAMA: Rafael Abednego Chayadi");
+        System.out.println("NIM: 607062330112");
+        System.out.println("Kelas: D3IF-47-04");
+    }
+}
